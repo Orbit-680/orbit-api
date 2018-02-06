bc0d7d6891cbcf5c1146fc9354c46f047445dc6f
brocktubre <brocktubre@gmail.com>
2018-02-05 22:34:35 -0600

    Try catch for creating role

--- src/main/java/com/csc680/orbit/recordmapper/UserRecordMapper.java
+++ src/main/java/com/csc680/orbit/recordmapper/UserRecordMapper.java
@@ -5,23 +5,23 @@ import static com.csc680.orbit.database.tables.Role.ROLE;
 
 import org.jooq.Record;
 import org.jooq.RecordMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.csc680.orbit.model.pojo.Role;
 import com.csc680.orbit.model.pojo.User;
-import com.csc680.orbit.repository.RoleRepository;
 
 public class UserRecordMapper implements RecordMapper<Record, User>{
 	
-	@Autowired
-	RoleRepository roleReposityory;
 	
 	@Override
 	public User map(Record r) {
-//		String id = r.getValue(USER.ROLE_ID).toString();
-//		Role role = roleReposityory.findOne(id);
-		// Role role = new Role(r.getValue(USER.ROLE_ID), r.getValue(ROLE.NAME));
-		Role role = new Role(r.getValue(USER.ROLE_ID));
+		Role role = null;
+		try{
+			role = new Role(r.getValue(USER.ROLE_ID), r.getValue(ROLE.NAME));
+		}
+		catch (Exception e){
+			role = new Role(r.getValue(USER.ROLE_ID));
+		}
+
 		return new User(
 				r.getValue(USER.ID),
 				r.getValue(USER.EMAIL),
