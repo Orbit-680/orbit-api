9bf92b428e8ea25b790614203b3bd009fb03e7e4
Brock Tubre <brock.tubre@csra.com>
2018-04-13 14:21:13 -0500

    Correct URL

--- src/main/java/com/csc680/orbit/utils/DBConfig.java
+++ src/main/java/com/csc680/orbit/utils/DBConfig.java
@@ -3,7 +3,7 @@ package com.csc680.orbit.utils;
 public class DBConfig {
 	public static final String USERNAME = "APIacc";
     public static final String PASSWORD = "KU5K3vxAAC";
-    // public static final String URL = "jdbc:mysql://18.220.78.140.brocktubre.com:3306/orbit";
+    // public static final String URL = "jdbc:mysql://18.220.78.140:3306/orbit";
     public static final String URL = "jdbc:mysql://18.219.141.160:3306/orbit";
     // public static final String URL = "jdbc:mysql://52.14.69.5:3306/orbit";
 	
